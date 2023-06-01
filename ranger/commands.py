4c340ea2ccb9afb45011d84462a9a6772ead8e89
ShellTux <115948079+ShellTux@users.noreply.github.com>
2023-06-01 23:21:17 +0100

    add ranger

---
+++ ranger/commands.py
@@ -0,0 +1,125 @@
+from __future__ import (absolute_import, division, print_function)
+# You can import any python module as needed.
+import os
+# You always need to import ranger.api.commands here to get the Command class:
+from ranger.api.commands import Command
+from ranger.core.loader import CommandLoader
+import subprocess
+import json
+import atexit
+import socket
+from pathlib import Path
+import logging
+logger = logging.getLogger(__name__)
+import traceback
+from ranger.ext.img_display import ImageDisplayer, register_image_displayer
+
+
+class emptyTrash(Command):
+    """:empty
+
+    Empties the trash directory ~/.local/share/Trash/files
+    """
+
+    def execute(self):
+        self.fm.run("rm -rf ${XDG_DATA_HOME:-$HOME/.local/share}/Trash/files")
+
+class extract_here(Command):
+    def execute(self):
+        """ extract selected files to current directory."""
+        cwd = self.fm.thisdir
+        marked_files = tuple(cwd.get_selection())
+
+        def refresh(_):
+            cwd = self.fm.get_directory(original_path)
+            cwd.load_content()
+
+        one_file = marked_files[0]
+        cwd = self.fm.thisdir
+        original_path = cwd.path
+        au_flags = ['-x', cwd.path]
+        au_flags += self.line.split()[1:]
+        au_flags += ['-e']
+
+        self.fm.copy_buffer.clear()
+        self.fm.cut_buffer = False
+        if len(marked_files) == 1:
+            descr = "extracting: " + os.path.basename(one_file.path)
+        else:
+            descr = "extracting files from: " + os.path.basename(
+                one_file.dirname)
+        obj = CommandLoader(args=['aunpack'] + au_flags
+                            + [f.path for f in marked_files], descr=descr,
+                            read=True)
+
+        obj.signal_bind('after', refresh)
+        self.fm.loader.add(obj)
+
+class convert_image(Command):
+    """ Convert image to another format """
+    # def execute(self):
+    #     cwd = self.fm.thisdir
+    #     marked_files = tuple(cwd.get_selection())
+
+    #     def refresh(_):
+    #         cwd = self.fm.get_directory(original_path)
+    #         cwd.load_content()
+    pass
+
+@register_image_displayer("mpv")
+class MPVImageDisplayer(ImageDisplayer):
+    """Implementation of ImageDisplayer using mpv, a general media viewer.
+    Opens media in a separate X window.
+
+    mpv 0.25+ needs to be installed for this to work.
+    """
+
+    def _send_command(self, path, sock):
+
+        message = '{"command": ["raw","loadfile",%s]}\n' % json.dumps(path)
+        s = socket.socket(socket.AF_UNIX, socket.SOCK_STREAM)
+        s.connect(str(sock))
+        logger.info('-> ' + message)
+        s.send(message.encode())
+        message = s.recv(1024).decode()
+        logger.info('<- ' + message)
+
+    def _launch_mpv(self, path, sock):
+
+        proc = subprocess.Popen([
+            * os.environ.get("MPV", "mpv").split(),
+            "--no-terminal",
+            "--force-window",
+            "--input-ipc-server=" + str(sock),
+            "--image-display-duration=inf",
+            "--loop-file=inf",
+            "--no-osc",
+            "--no-input-default-bindings",
+            "--keep-open",
+            "--idle",
+            "--",
+            path,
+        ])
+
+        @atexit.register
+        def cleanup():
+            proc.terminate()
+            sock.unlink()
+
+    def draw(self, path, start_x, start_y, width, height):
+
+        path = os.path.abspath(path)
+        cache = Path(os.environ.get("XDG_CACHE_HOME", "~/.cache")).expanduser()
+        cache = cache / "ranger"
+        cache.mkdir(exist_ok=True)
+        sock = cache / "mpv.sock"
+
+        try:
+            self._send_command(path, sock)
+        except (ConnectionRefusedError, FileNotFoundError):
+            logger.info('LAUNCHING ' + path)
+            self._launch_mpv(path, sock)
+        except Exception as e:
+            logger.exception(traceback.format_exc())
+            sys.exit(1)
+        logger.info('SUCCESS')
