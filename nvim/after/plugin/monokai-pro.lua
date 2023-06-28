1061a4378137ce6591bcae50a2b9c8f428cc9c66
ShellTux <115948079+ShellTux@users.noreply.github.com>
2023-06-28 20:04:51 +0100

    refactor nvim colorschemes lua config files

--- nvim/after/plugin/monokai-pro.lua
+++ nvim/after/plugin/monokai-pro.lua
@@ -2,8 +2,7 @@ require('monokai-pro').setup({
 	transparent_background = true,
 	day_night = {
 		enable = true, -- turn off by default
-		day_filter = "pro", -- classic | octagon | pro | machine | ristretto | spectrum
-		night_filter = "spectrum", -- classic | octagon | pro | machine | ristretto | spectrum
+		day_filter = 'pro', -- classic | octagon | pro | machine | ristretto | spectrum
+		night_filter = 'ristretto', -- classic | octagon | pro | machine | ristretto | spectrum
 	},
-	inc_search = "underline", -- underline | background
 })
--- nvim/after/plugin/tokyonight.lua
+++ nvim/after/plugin/tokyonight.lua
@@ -1,1 +1,3 @@
-require('tokyonight').setup()
+require('tokyonight').setup({
+	transparent = true,
+})
