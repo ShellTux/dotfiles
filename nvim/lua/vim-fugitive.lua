99dd6775cc73f7396423cf7c14bdf336cf2317d9
ShellTux <115948079+ShellTux@users.noreply.github.com>
2023-06-28 21:51:42 +0100

    feat(nvim): add git conflict keybinding
    Now when pressing leader+gcon
    It will prompt with telescope to type the unmerged conflicted file

--- nvim/lua/vim-fugitive.lua
+++ nvim/lua/vim-fugitive.lua
@@ -1,3 +1,36 @@
+local has_telescope, _ = pcall(require, 'telescope')
+local telescope_find_files
+if has_telescope then
+	telescope_find_files = require('telescope.builtin').find_files
+end
+
+local function findGitConflictFiles()
+	local git_conflict_command = { 'git', 'diff', '--name-only', '--diff-filter', 'U' }
+	if has_telescope then
+		telescope_find_files({
+			find_command = git_conflict_command,
+			prompt_title = 'Git conflicts',
+			attach_mappings = function(_, map)
+				map("n", "<cr>", exec_dap)
+				map("i", "<cr>", exec_dap)
+				return true
+			end,
+		})
+	else
+		local handle = io.popen(table.concat(git_conflict_command, ' '))
+		local result = handle:read("*a")
+		handle:close()
+
+		if result ~= '' then
+			local command = string.format('echo "%s"',
+			'Git conflict files found:\n' .. result)
+			vim.api.nvim_command(command)
+		else
+			print('No git conflict files found.')
+		end
+	end
+end
+
 local function gitdiff(history)
 	return function()
 		vim.cmd.tabnew('%')
@@ -33,6 +66,7 @@ local module = {
 	add      = command('Git add %'),
 	checkout = command('Git checkout'),
 	commit   = command('Git commit'),
+	conflict = findGitConflictFiles,
 	diff     = gitdiff,
 	diff_staged = openCommandInNewTab('Git diff --staged'),
 	git      = openCommandInNewTab('Git'),
