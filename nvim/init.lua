d63f0f6255e5b5d05e1230e3bd59f77039376961
ShellTux <115948079+ShellTux@users.noreply.github.com>
2023-07-09 06:00:51 +0100

    feat(nvim): Add vim-visual-multi plugin
    This commit updates the `init.lua` and `plugins.lua` files in the nvim directory.
    
    In `init.lua`, the following changes were made:
    - Removed comments for each setting to improve readability.
    - Reorganized the settings for better clarity.
    
    In `plugins.lua`, the following change was made:
    - Added the `vim-visual-multi` plugin to the list of installed plugins.

--- nvim/init.lua
+++ nvim/init.lua
@@ -5,14 +5,14 @@ require('autocmds')
 ------------
 --- Settings
 ------------
-vim.cmd('syntax enable') -- Syntax highlighting
-vim.cmd('set wildmenu') -- Command-line completion (Tab to invoke completion) and a menu pops up
-vim.cmd('set ignorecase smartcase ') -- Ignores case when the pattern contains lowercase only, otherwise will try to exact match
-vim.cmd('set incsearch') -- While typing a search command, show where the pattern, as it was typed	so far, matches. The matched string is highlighted. If the pattern is invalid or not found, nothing is shown.
-vim.cmd('set hlsearch') -- When there is a previous search pattern, highlight all its matches.
-vim.cmd('set number') -- precede each line with its line number.
-vim.cmd('set encoding=UTF-8') -- Sets the character encoding used inside Vim.
-vim.cmd('set scrolloff=999') -- Minimal number of screen lines to keep above and below the cursor.
+vim.cmd('syntax enable')
+vim.cmd('set wildmenu')
+vim.cmd('set ignorecase smartcase ')
+vim.cmd('set incsearch')
+vim.cmd('set hlsearch')
+vim.cmd('set number')
+vim.cmd('set encoding=UTF-8')
+vim.cmd('set scrolloff=999')
 vim.cmd('set termguicolors')
 vim.cmd('set wrap')
 vim.cmd('set linebreak')
