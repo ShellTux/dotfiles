7a0792609c93a16b208e5ce7037fe09bcb202d9d
ShellTux <115948079+ShellTux@users.noreply.github.com>
2023-08-13 00:57:30 +0100

    feat(nvim, vim): system-ctl --user daemon reload on service change

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,2 +1,3 @@
+vim.cmd("autocmd BufWritePost *.service !systemctl --user daemon-reload")
 vim.cmd("autocmd BufWritePost sxhkdrc !systemctl restart --user sxhkd")
 vim.cmd("autocmd BufWritePost config.def.h !doas make clean install")
