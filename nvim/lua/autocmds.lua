3548d8a389a0bc5c2594eaf034dbd993d1f4c286
ShellTux <115948079+ShellTux@users.noreply.github.com>
2023-08-13 01:59:54 +0100

    feat(nvim,vim): restart hotkeys service on swhkdrc change

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -1,3 +1,4 @@
 vim.cmd("autocmd BufWritePost *.service !systemctl --user daemon-reload")
 vim.cmd("autocmd BufWritePost sxhkdrc !systemctl restart --user sxhkd")
+vim.cmd("autocmd BufWritePost swhkdrc !systemctl restart --user hotkeys")
 vim.cmd("autocmd BufWritePost config.def.h !doas make clean install")
