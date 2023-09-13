f79dbe88822954afe4a1a9668aa1e88be52dc25c
ShellTux <115948079+ShellTux@users.noreply.github.com>
2023-09-13 04:40:54 +0100

    add ytfzf config

---
+++ ytfzf/conf.sh
@@ -0,0 +1,11 @@
+# shellcheck disable=SC2034
+#Variables {{{
+show_thumbnails=1
+##}}}
+#
+##Functions {{{
+#external_menu () {
+#    #use rofi instead of dmenu
+#    rofi -dmenu -width 1500 -p "$1"
+#}
+#}}}
