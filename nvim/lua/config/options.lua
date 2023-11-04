ee38f2d3493f9dff2af63576f1b99cc3bc90c795
ShellTux <115948079+ShellTux@users.noreply.github.com>
2023-11-04 20:53:57 +0000

    format nvim/lua/config/options.lua

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,33 +1,33 @@
 -- vim.opt.tabstop = 2
-vim.opt.wildignore = { '*.o', '*.a', '__pycache__' }
-vim.opt.listchars = { tab = '> ', trail = '•', nbsp = '~' }
-vim.opt.wildmenu = true
-vim.opt.syntax = 'enabled'
-vim.opt.encoding = 'UTF-8'
+vim.opt.wildignore     = { '*.o', '*.a', '__pycache__' }
+vim.opt.listchars      = { tab = '> ', trail = '•', nbsp = '~' }
+vim.opt.wildmenu       = true
+vim.opt.syntax         = 'enabled'
+vim.opt.encoding       = 'UTF-8'
 
 -- Text Display
-vim.opt.linebreak = true
-vim.opt.scrolloff = 999
-vim.opt.sidescrolloff = 10
-vim.opt.textwidth = 80
+vim.opt.linebreak      = true
+vim.opt.scrolloff      = 999
+vim.opt.sidescrolloff  = 10
+vim.opt.textwidth      = 80
 
 -- Tab / Indentation
-vim.opt.wrap = false
+vim.opt.wrap           = false
 
 -- Search
-vim.opt.incsearch = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.hlsearch = true
+vim.opt.incsearch      = true
+vim.opt.ignorecase     = true
+vim.opt.smartcase      = true
+vim.opt.hlsearch       = true
 
 -- Appearance
-vim.opt.number = true
+vim.opt.number         = true
 vim.opt.relativenumber = false
-vim.opt.termguicolors = true
+vim.opt.termguicolors  = true
 -- vim.opt.colorcolumn = '100'
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn     = 'yes'
 -- vim.opt.completeopt = 'menuone,noinsert,noselect'
-vim.opt.background = 'dark'
+vim.opt.background     = 'dark'
 
 -- Behaviour
 vim.opt.mouse:append('a')
