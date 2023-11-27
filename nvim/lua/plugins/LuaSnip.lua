53c342864a768c0cf8403b74aec55079c961ec90
ShellTux <115948079+ShellTux@users.noreply.github.com>
2023-11-27 17:51:43 +0000

    fix(luasnip): enable tab in insert mode to insert tab

--- nvim/lua/plugins/LuaSnip.lua
+++ nvim/lua/plugins/LuaSnip.lua
@@ -15,8 +15,8 @@ return {
 		if opts then ls.config.setup(opts) end
 
 		vim.tbl_map(
-		function(type) require("luasnip.loaders.from_" .. type).lazy_load() end,
-		{ "vscode", "snipmate", "lua" }
+			function(type) require("luasnip.loaders.from_" .. type).lazy_load() end,
+			{ "vscode", "snipmate", "lua" }
 		)
 
 		-- friendly-snippets - enable standardized comments snippets
@@ -37,14 +37,14 @@ return {
 	keys = function()
 		local ls = require('luasnip')
 
-		vim.keymap.set({'i'}, '<C-K>', function() ls.expand() end, {silent = true})
-		vim.keymap.set({'i', 's'}, '<Tab>', function() ls.jump( 1) end, {silent = true})
-		vim.keymap.set({'i', 's'}, '<S-Tab>', function() ls.jump(-1) end, {silent = true})
+		vim.keymap.set({ 'i' }, '<C-K>', function() ls.expand() end, { silent = true })
+		vim.keymap.set({ 'i', 's' }, '<C-j>', function() ls.jump(1) end, { silent = true })
+		vim.keymap.set({ 'i', 's' }, '<C-k>', function() ls.jump(-1) end, { silent = true })
 
-		vim.keymap.set({'i', 's'}, '<C-E>', function()
+		vim.keymap.set({ 'i', 's' }, '<C-E>', function()
 			if ls.choice_active() then
 				ls.change_choice(1)
 			end
-		end, {silent = true})
+		end, { silent = true })
 	end,
 }
