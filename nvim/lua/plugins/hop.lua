afce42d72db35e65ce0445ef7e3668676224281f
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-01-09 17:49:43 +0000

    chore(nvim/hop.lua): search beyond current line

--- nvim/lua/plugins/hop.lua
+++ nvim/lua/plugins/hop.lua
@@ -1,24 +1,25 @@
-return	{
+return {
 	'phaazon/hop.nvim',
 	branch = 'v2', -- optional but strongly recommended
 	lazy = false,
 	opts = {
-		keys = 'etovxqpdygfblzhckisuran'
+		keys = 'etovxqpdygfblzhckisuran',
+		current_line_only = false,
 	},
 	keys = function()
 		local hop = require('hop')
 		local directions = require('hop.hint').HintDirection
 		vim.keymap.set('', 'f', function()
-			hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true })
-		end, {remap=true})
+			hop.hint_char1({ direction = directions.AFTER_CURSOR })
+		end, { remap = true })
 		vim.keymap.set('', 'F', function()
-			hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true })
-		end, {remap=true})
+			hop.hint_char1({ direction = directions.BEFORE_CURSOR })
+		end, { remap = true })
 		vim.keymap.set('', 't', function()
-			hop.hint_char1({ direction = directions.AFTER_CURSOR, current_line_only = true, hint_offset = -1 })
-		end, {remap=true})
+			hop.hint_char1({ direction = directions.AFTER_CURSOR, hint_offset = -1 })
+		end, { remap = true })
 		vim.keymap.set('', 'T', function()
-			hop.hint_char1({ direction = directions.BEFORE_CURSOR, current_line_only = true, hint_offset = 1 })
-		end, {remap=true})
+			hop.hint_char1({ direction = directions.BEFORE_CURSOR, hint_offset = 1 })
+		end, { remap = true })
 	end,
 }
