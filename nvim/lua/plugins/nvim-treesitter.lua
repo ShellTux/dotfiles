f48bc01b8738be37621f029f3bcffbdad34dba28
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-01-16 15:50:23 +0000

    feat(nvim-treesitter): update parsers on plugin update

--- nvim/lua/plugins/nvim-treesitter.lua
+++ nvim/lua/plugins/nvim-treesitter.lua
@@ -22,6 +22,10 @@ local nvim_treesitter_options = {
 return {
 	'nvim-treesitter/nvim-treesitter',
 	lazy = false,
+	build = function()
+		local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+		ts_update()
+	end,
 	config = function()
 		require('nvim-treesitter.configs').setup(nvim_treesitter_options)
 	end,
