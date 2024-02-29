4cb433fd842cdc4dfc4d62ec951b03f76c8f8aa9
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-02-29 21:40:33 +0000

    chore(nvim-silicon.lua): change pad_horiz and pad_vert

--- nvim/lua/plugins/nvim-silicon.lua
+++ nvim/lua/plugins/nvim-silicon.lua
@@ -13,6 +13,8 @@ return {
 			background = '#94e2d5',
 			theme = 'OneHalfDark',
 			font = 'FiraCode Nerd Font',
+			pad_horiz = 40,
+			pad_vert = 50,
 			window_title = function()
 				return vim.fn.fnamemodify(
 					vim.api.nvim_buf_get_name(
