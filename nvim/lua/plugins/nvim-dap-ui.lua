99e8763bc9acf7a29d9988da63adcb06a8700c6f
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-03-26 21:20:50 +0000

    fix(nvim/nvim-dap-ui.lua): add new required dependencies

--- nvim/lua/plugins/nvim-dap-ui.lua
+++ nvim/lua/plugins/nvim-dap-ui.lua
@@ -1,7 +1,10 @@
 return {
 	'rcarriga/nvim-dap-ui',
 	event = 'VeryLazy',
-	dependencies = 'mfussenegger/nvim-dap',
+	dependencies = {
+		'mfussenegger/nvim-dap',
+		'nvim-neotest/nvim-nio',
+	},
 	config = function()
 		local dap, dapui = require('dap'), require('dapui')
 		local open       = function() dapui.open() end
