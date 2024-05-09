e5c8d6bee6711726749c8b02d99fae47482abbcf
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-05-09 17:32:07 +0100

    refactor(codeium.lua): add codeium.vim as plugin replacement for codeium.nvim

--- nvim/lua/plugins/codeium.lua
+++ nvim/lua/plugins/codeium.lua
@@ -1,22 +1,45 @@
-local keyOptions = { expr = true }
+local keyOptions = { expr = true, silent = true }
 vim.g.codeium_enabled = false
+vim.g.codeium_vim = true
+vim.g.codeium_disable_bindings = 0
 
-return {
-	'Exafunction/codeium.nvim',
-	dependencies = {
-		'nvim-lua/plenary.nvim',
-		'hrsh7th/nvim-cmp',
-	},
-	enabled = false,
-	event = 'BufEnter',
-	keys = {
-		vim.keymap.set('i', '<C-g>', vim.fn['codeium#Accept'], keyOptions),
-		vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end, keyOptions),
-		vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end, keyOptions),
-		vim.keymap.set('i', '<c-x>', vim.fn['codeium#Clear'], keyOptions),
-	},
-	config = function()
-		require('codeium').setup({
-		})
-	end
-}
+if vim.g.codeium_vim then
+	return {
+		'Exafunction/codeium.vim',
+		enabled = false,
+		event = 'BufEnter',
+		config = function()
+			-- Change '<C-g>' here to any keycode you like.
+			vim.keymap.set('i', '<C-g>', function() return vim.fn['codeium#Accept']() end,
+				keyOptions)
+			vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end,
+				keyOptions)
+			vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
+				keyOptions)
+			vim.keymap.set('i', '<c-x>', function() return vim.fn['codeium#Clear']() end,
+				keyOptions)
+		end
+	}
+else
+	return {
+		'Exafunction/codeium.nvim',
+		dependencies = {
+			'nvim-lua/plenary.nvim',
+			'hrsh7th/nvim-cmp',
+		},
+		enabled = false,
+		event = 'BufEnter',
+		keys = {
+			vim.keymap.set('i', '<C-g>', vim.fn['codeium#Accept'], keyOptions),
+			vim.keymap.set('i', '<c-;>', function() return vim.fn['codeium#CycleCompletions'](1) end,
+				keyOptions),
+			vim.keymap.set('i', '<c-,>', function() return vim.fn['codeium#CycleCompletions'](-1) end,
+				keyOptions),
+			vim.keymap.set('i', '<c-x>', vim.fn['codeium#Clear'], keyOptions),
+		},
+		config = function()
+			require('codeium').setup({
+			})
+		end
+	}
+end
