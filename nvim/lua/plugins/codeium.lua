441034a83df434a88d1de63b1b31d00de11cd357
ShellTux <115948079+ShellTux@users.noreply.github.com>
2024-05-11 16:06:10 +0100

    chore(codeium.vim): only enable for certain filetypes

--- nvim/lua/plugins/codeium.lua
+++ nvim/lua/plugins/codeium.lua
@@ -2,11 +2,30 @@ local keyOptions = { expr = true, silent = true }
 vim.g.codeium_enabled = false
 vim.g.codeium_vim = true
 vim.g.codeium_disable_bindings = 0
+vim.g.codeium_filetypes_disabled_by_default = true
+vim.g.codeium_filetypes = {
+	bash = true,
+	css = true,
+	c = true,
+	cpp = true,
+	dart = true,
+	go = true,
+	html = true,
+	javascript = true,
+	java = true,
+	kotlin = true,
+	lua = true,
+	php = true,
+	python = true,
+	rust = true,
+	sh = true,
+	swift = true,
+	typescript = true,
+}
 
 if vim.g.codeium_vim then
 	return {
 		'Exafunction/codeium.vim',
-		enabled = false,
 		event = 'BufEnter',
 		config = function()
 			-- Change '<C-g>' here to any keycode you like.
@@ -27,7 +46,6 @@ else
 			'nvim-lua/plenary.nvim',
 			'hrsh7th/nvim-cmp',
 		},
-		enabled = false,
 		event = 'BufEnter',
 		keys = {
 			vim.keymap.set('i', '<C-g>', vim.fn['codeium#Accept'], keyOptions),
